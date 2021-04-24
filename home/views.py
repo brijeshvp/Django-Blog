87697065d2f5eff73b04605a72f7ab268fcbb4e3
Brijesh <brijeshpeshvani38@gmail.com>
2021-04-24 23:08:13 +0530

    Added bootstrap and template inheritance as a seperate htmls for both the apps

--- home/views.py
+++ home/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('This is home')
+    # return HttpResponse('This is home')
+    return render(request,'home/home.html')
 
 def about(request):
-    return HttpResponse('This is about')
+    # return HttpResponse('This is about')
+    return render(request,'home/about.html')
+
 
 def contact(request):
-    return HttpResponse('This is contact')
+    # return HttpResponse('This is contact')
+    return render(request,'home/contact.html')
