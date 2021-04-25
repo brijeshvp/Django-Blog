3853540f979f61f36b258d0f685f1552bd079ecf
Brijesh <brijeshpeshvani38@gmail.com>
2021-04-25 22:11:09 +0530

    Added Contact model and registered it in admin.py and also registered both the apps in INSTALLED_APPS in settings.py also applied migrations and migrate command to see changes of contact model in database

--- blog_proj/settings.py
+++ blog_proj/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Registered apps by me(for my project)
+    'home.apps.HomeConfig', 
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
